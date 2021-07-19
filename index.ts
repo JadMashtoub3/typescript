225d994d36693029ea8ec59f41c2e3aafd825c49
JadMashtoub3 <103596586@student.swin.edu.au>
2021-07-19 11:19:05 +1000

    2 files modified

--- index.ts
+++ index.ts
@@ -4,8 +4,14 @@
 // Write TypeScript code!
 //const appDiv: HTMLElement = document.getElementById('app');
 //appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;   
+const output: HTMLElement = document.getElementById("Output");
+const button: HTMLElement = document.getElementById("Custom-btn");
+button.addEventListener('click', fizzbuzz);
 
 function fizzbuzz() {
+  const startNumInput : HTMLInputElement = <HTMLInputElement> {
+    document.getElementById("start-num")
+  }
   for (let i = 1; i <=100; i++) {
     if (i % 3 ==0 && i %5 == 0) {
       console.log('fizzbuzz', i);
